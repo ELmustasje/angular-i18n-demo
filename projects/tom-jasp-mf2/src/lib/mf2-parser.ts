7dba7d1873fb1da46f22a407806ea08e6f4a0972
jesperkha <hammerjesp@gmail.com>
2025-09-12 10:56:41 +0200

    Functional approach

--- projects/tom-jasp-mf2/src/lib/mf2-parser.ts
+++ projects/tom-jasp-mf2/src/lib/mf2-parser.ts
@@ -1,42 +1,32 @@
-// mf2-parser.ts
-import { input } from '@angular/core';
 import { MessageFormat } from 'messageformat';
 
-export class Mf2Parser {
-  private mf: MessageFormat;
+export default mf2ToHtml;
 
-  constructor(msg: MessageFormat) {
-    this.mf = msg;
-  };
+function mf2ToHtml(s: string, vars?: Record<string, any>): string {
+    const mf = new MessageFormat('en', s);
+    const parts = mf.formatToParts(vars);
+    const result = [];
 
-  getHtml() {
-    const parts = this.mf.formatToParts();
-
-    let result = '';
     for (const part of parts) {
-      if (part.type == "text") {
-        result += part.value
-      }
-      if (part.type == "markup") {
-        if (part.kind == "open") {
-          switch (part.name) {
-            case "bold": {
-              result += "<b>"
-            }
-          }
+        switch (part.type) {
+            case 'text':
+                result.push(part.value);
+                break;
+
+            case 'markup':
+                result.push(markupToHtml(part.name, part.kind));
+                break;
         }
-        if (part.kind == "close") {
-          switch (part.name) {
-            case "bold": {
-              result += "</b>"
-            }
-          }
-        }
-      }
     }
 
-    console.log(result);
+    return result.join('');
+}
 
-    return result;
-  }
+function markupToHtml(name: string, kind: string): string {
+    switch (name) {
+        case 'bold':
+            return kind === 'open' ? '<b>' : '</b>';
+    }
+
+    return '';
 }
